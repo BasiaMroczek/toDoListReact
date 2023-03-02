a23c824be735f65bf8dff31f51610b51cddd0799
Barbara Mroczek <method.kontakt@gmail.com>
2023-03-02 15:15:25 +0100

    first commit

---
+++ src/Tasks/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./style.css";
+
+const Tasks = ({ tasks, hideDoneTasks }) => (
+    <ul className="tasks">
+        {tasks.map(task => (
+            <li
+                className={`tasks__item${task.done && hideDoneTasks
+                        ? " tasks__item--hidden"
+                        : ""}`
+                }
+            >
+                <button className="task__button task__button--done">
+                    {task.done ? "✔" : ""}
+                </button>
+                <span
+                    className={`"list__item ${task.done ? "list__item--done" : ""}`}>
+                    {task.content}
+                </span>
+                <button className="task__button task__button--remove">🗑</button>
+            </li>
+        ))}
+    </ul>
+);
+
+export default Tasks;
