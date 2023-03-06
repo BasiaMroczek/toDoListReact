5e2e025b9cf4941cec7e52887b7a9aeca06d4fe0
Barbara Mroczek <method.kontakt@gmail.com>
2023-03-06 15:18:59 +0100

    remove task interaction

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,23 +1,29 @@
 import React from "react";
 import "./style.css";
 
-const Tasks = ({ tasks, hideDone }) => (
+const Tasks = ({ tasks, hideDone, removeTask }) => (
     <ul className="tasks">
         {tasks.map(task => (
             <li
+                key={task.id}
                 className={`tasks__item${task.done && hideDone
                     ? " tasks__item--hidden"
                     : ""}`
                 }
             >
-                <button className="task__button task__button--done">
+                <button className="tasks__button tasks__button--toggleDone">
                     {task.done ? "✔" : ""}
                 </button>
-                <span
-                    className={`"list__item ${task.done ? "list__item--done" : ""}`}>
+                <span className={
+                    `tasks__content${task.done ? " tasks__content--done" : ""}`
+                }>
                     {task.content}
                 </span>
-                <button className="task__button task__button--remove">🗑</button>
+                <button className="tasks__button tasks__button--remove"
+                onClick={() => removeTask(task.id)}
+                >
+                    🗑
+                </button>
             </li>
         ))}
     </ul>
